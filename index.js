3a4a5ac687d8dcfa91f556e73b6268f45c905690
moira56 <mcekada1@student.unipu.hr>
2024-11-23 10:51:37 +0100

    index.js

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Pocetna');
 });
-app.get('/zaposlenici', (req, res) => {
+
+app.get('/zaposlenici/:id', (req, res) => {
+    const id_zaposlenika = req.params.id;
     fs.readFile('zaposlenici.json', 'utf8', (err, data) => {
         if (err) {
-            return res.status(500).json({ message: 'Greška (čitanje podataka).' });
+            return res.status(500).json({ message: 'Greška kod čitanja podataka' });
         }
-        const zaposlenici = JSON.parse(data);
-        res.json(zaposlenici);
-    });
+        const zaposlenici = JSON.parse(data); 
+        const zaposlenik = zaposlenici.find(zaposlenik => zaposlenik.id == id_zaposlenika);
+        if (zaposlenik) {
+            res.json(zaposlenik);
+        } else {
+            res.json({ message: 'Zaposlenik s tim id-em ne postoji' });
+        }
+    });
 });
+
 app.listen(PORT, error => {
     if(error) {
         console.error(`Greška: ${error.message}`);
